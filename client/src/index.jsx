254978b5fdee506b0c21cc67a521f4f0a9c83d6b
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-27 11:31:52 -0700

    "added react google maps to index.html"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,9 @@
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import {
+  withGoogleMap,
+  GoogleMap,
+  InfoWindow,
+  Marker
+} from 'react-google-maps';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -11,12 +15,18 @@ class Index extends React.Component {
   render() {
     return (
       <div>
+        <div id="map">
+          <Map google={this.props.google} />
+        </div>
         <p> This is the index component change for app.json test #4</p>
       </div>
     );
   }
 }
 
-// export default Index;
+export default GoogleApiComponent({
+  apiKey: 'AIzaSyBViLR1RDD5f4OmqbD2UH0QTwyYsjk8ldE'
+})(Index);
+
 
 ReactDOM.render(<Index />, document.getElementById('app'));
