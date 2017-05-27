ccddbd1c7cba32157511bc695761c2e0a86b2f34
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-27 15:59:12 -0700

    "can add markers to map (no map function yet)"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,26 +9,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
+window.initMap = function() {
+  var bakerbeach = {lat: 37.7656, lng: -122.511827};
+  var ggb = {lat: 37.807, lng: -122.476};
+  var superburgers = {lat: 37.762, lng: -122.434};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: superburgers
+  });
+  var marker1 = new google.maps.Marker({
+    position: bakerbeach,
+    map: map
+  });
+  var marker2 = new google.maps.Marker({
+    position: ggb,
+    map: map
+  });
+  var marker3 = new google.maps.Marker({
+    position: superburgers,
+    map: map
+  });
+};
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
   }
-  initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
+
   render() {
     return (
       <div>
-        {this.initMap()}
+        {window.initMap()}
       </div>
     );
   }
