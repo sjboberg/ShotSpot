d2b74439f875fc67796f307f508e7cbee2fef38a
Sebastian Boberg <sebastianboberg@gmail.com>
2017-06-02 10:49:47 -0700

    Queries must be double-qouted to support inner single quotes

--- server/models/dbHelpers.js
+++ server/models/dbHelpers.js
@@ -1,7 +1,7 @@
 var pool = require('./dbConnection.js');
 
 exports.addUser = (username, cb) => {
-  var query = 'INSERT INTO users (username) VALUES ($1);';
+  var query = "INSERT INTO users (username) VALUES ($1);";
   pool.query(query, [username], function (err, result) {
     if (err) {
       cb(err, null);
@@ -12,7 +12,7 @@ exports.addUser = (username, cb) => {
 };
 
 exports.addLocation = (name, coordinates, cb) => {
-  var query = 'INSERT INTO locations (name, coordinates) VALUES ($1, $2);';
+  var query = "INSERT INTO locations (name, coordinates) VALUES ($1, $2);";
   pool.query(query, [name, coordinates], function (err, result) {
     if (err) {
       cb(err, null);
@@ -23,7 +23,7 @@ exports.addLocation = (name, coordinates, cb) => {
 };
 
 exports.addPhoto = (locationId, userId, uri, cb) => {
-  var query = 'INSERT INTO photos (location_id, user_id, uri, date) VALUES ($1, $2, $3, CURRENT_TIMESTAMP);';
+  var query = "INSERT INTO photos (location_id, user_id, uri, date) VALUES ($1, $2, $3, CURRENT_TIMESTAMP);";
   pool.query(query, [locationId, userId, uri], function (err, result) {
     if (err) {
       cb(err, null);
@@ -34,7 +34,7 @@ exports.addPhoto = (locationId, userId, uri, cb) => {
 };
 
 exports.getPhotoUrl = (photoId, cb) => {
-  var query = 'SELECT uri FROM photos WHERE id = $1;';
+  var query = "SELECT uri FROM photos WHERE id = $1;";
   pool.query(query, function (err, result) {
     if (err) {
       cb(err, null);
@@ -45,7 +45,7 @@ exports.getPhotoUrl = (photoId, cb) => {
 };
 
 exports.getAllPhotos = (cb) => {
-  var query = 'SELECT * FROM public.photos';
+  var query = "SELECT * FROM public.photos";
   pool.query(query, function(err, result) {
     if (err) {
       cb(err, null);
@@ -56,7 +56,7 @@ exports.getAllPhotos = (cb) => {
 };
 
 exports.addLocationComment = (locationId, userId, content, cb) => {
-  var query = 'INSERT INTO comments (location_id, user_id, content, date) VALUES ($1, $2, $3, CURRENT_TIMESTAMP);';
+  var query = "INSERT INTO comments (location_id, user_id, content, date) VALUES ($1, $2, $3, CURRENT_TIMESTAMP);";
   pool.query(query, [locationId, userId, content], function (err, result) {
     if (err) {
       cb(err, null);
