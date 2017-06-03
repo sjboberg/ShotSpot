0de605807679b2f4d20d6be84af49dda2447a1a5
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-02 19:04:16 -0700

    "created components for individual component page"

---
+++ client/src/IndivLocation/Photographs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+class Photographs extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3> This is the photograph section </h3>
+        
+      </div>
+    );
+  }
+}
+
+export default Photographs;
