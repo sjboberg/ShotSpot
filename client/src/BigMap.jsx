dae88be6205c89c350934c9bb53234457ae43b94
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-10 09:43:24 -0700

    "now popup works on bigmap"

--- client/src/BigMap.jsx
+++ client/src/BigMap.jsx
@@ -51,11 +51,12 @@ class BigMap extends React.Component {
             url="https://api.mapbox.com/styles/v1/fabbous/cj3gnpyq200112rtiabmb608s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiYm91cyIsImEiOiJjajNnbmlmNmQwMDRlMnFxc3Nwdms0dGV1In0.3IAYFLfwY1Z_eh1OxEognA"
             attribution="<attribution>" />
             {(this.state.objects[0] !== '...Loading') ? objects.map((location, i) => {
+              console.log("these are locations from inside the marker map");
               return (
                 <div key={i}>
                   <Marker position={[location.coordinates.latitude, location.coordinates.longitude]}>
                     <Popup>
-                      <span>{location.category}</span>
+                      <span>{location.name}</span>
                     </Popup>
                   </Marker>
                 </div>
