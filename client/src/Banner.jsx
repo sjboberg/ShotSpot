c8833696b333b4c95dfc9752df1fda208a25ebbb
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-06-10 21:03:57 -0700

    new banner componenet for ShotSpots header

---
+++ client/src/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Banner extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container" id="banner">
+
+            <b>SpotShots</b>
+  
+      </div>
+    );
+  }
+}
+
+export default Banner;
