b9c957c975ada264e18162144831c522fe65db1f
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-11 05:06:48 -0700

    "location names only appear once in the filter dropdown"

--- client/src/Filter.jsx
+++ client/src/Filter.jsx
@@ -6,10 +6,16 @@ class Filter extends React.Component {
   }
 
   render() {
+    let locNames = {};
+    (this.props.coordObjs.length > 1) ? this.props.coordObjs.forEach((value, i) => {
+      locNames[value.category] = i;
+    }) : console.log('Waiting to get data');
+
+    console.log('These are the location names: ', locNames);
     return (
       <div className="filter">
         <select value={this.props.initVal} onChange={this.props.handleChangeFilter}>
-          {this.props.coordObjs.map((value, i) => { return <option key={i} value={value.category}>{value.category}</option>; })}
+          {Object.keys(locNames).map((value, i) => { return <option key={i} value={value}>{value}</option>; })}
         </select>
       </div>
     );
