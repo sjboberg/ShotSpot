57f240fd6ad9ac76ac86507e19ce4a53b4c2e9c1
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-12 12:06:37 -0700

    "removed console logs from testing filter default values"

--- client/src/Filter.jsx
+++ client/src/Filter.jsx
@@ -14,8 +14,6 @@ class Filter extends React.Component {
     return (
       <div className="filter">
         <select value={this.props.initVal} onChange={this.props.handleChangeFilter}>
-          {console.log(Object.keys(locNames)[1], ' This is a locname')}
-          {console.log(this.props.initValue, ' This is the init val')}
           {Object.keys(locNames).map((value, i) => { return (this.props.initValue !== value) ? <option key={i} value={value}>{value}</option> : <option key={i} value={value} selected  >{value}</option>})}
         </select>
       </div>
