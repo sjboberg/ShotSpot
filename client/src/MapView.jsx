c6808d7f881aa3de7ad30ef06fb4eb04b81893ad
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-06-12 13:42:48 -0700

    removed map popup

--- client/src/MapView.jsx
+++ client/src/MapView.jsx
@@ -19,9 +19,6 @@ export class MapView extends React.Component {
             url="https://api.mapbox.com/styles/v1/fabbous/cj3gnpyq200112rtiabmb608s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFiYm91cyIsImEiOiJjajNnbmlmNmQwMDRlMnFxc3Nwdms0dGV1In0.3IAYFLfwY1Z_eh1OxEognA"
             attribution="<attribution>" />
             <Marker position={this.props.searchCoordinates}>
-            <Popup>
-              <span>Hi Everyone <br /> This is map of SF.</span>
-            </Popup>
             </Marker>
         
         </Map>
