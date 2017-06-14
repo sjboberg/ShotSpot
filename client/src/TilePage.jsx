d9db7bf227d212f7c3adfec1b4690ecd16b8f88f
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-06-13 21:07:02 -0700

    nav now visible

--- client/src/TilePage.jsx
+++ client/src/TilePage.jsx
@@ -85,7 +85,9 @@ class TilePage extends React.Component {
       return <Redirect push to={{pathname: '/Location/' + this.state.locSelect + '/' + this.props.location.state.stringy, state: {locSelect: this.state.locSelect, Latitude: this.props.location.state.Latitude, Longitude: this.props.location.state.Longitude}}} />;
     } else {
       return (
+
         <div id="tile">
+        <Navigation />
           <MapView searchCoordinates={this.state.searchCoordinates}/>
          <div className="container-fluid-fullwidth">
          <h4 onClick={this.handleMapClick.bind(this)}>Click me for mapview!</h4>
@@ -104,7 +106,7 @@ class TilePage extends React.Component {
 
           
          
-          {/*<Navigation />*/}
+          
            {(this.state.objects !== ['...Loading']) ? tempObjects.map((object) => {
             return (
               <div key={object.coverPhoto}>
