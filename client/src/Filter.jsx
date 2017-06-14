06a2482d8c53eb749b0feb8c4fad613cdabf33df
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-14 14:56:03 -0700

    "added click map to add location to bigmap and added a margin for that h5 tag"

--- client/src/Filter.jsx
+++ client/src/Filter.jsx
@@ -26,6 +26,7 @@ class Filter extends React.Component {
         <select name='categories' className="ui fluid dropdown" multiple="" id="filter-button"value={this.props.initValue} onChange={this.props.handleChangeFilter}>
           {Object.keys(locNames).map((value, i) => { return (this.props.initValue !== value) ? <option key={i} value={value}>{value}</option> : <option key={i} value={value} selected  >{value}</option>})}
         </select>
+        <h5 className="Mapnotifier">Click Map to add location!</h5>
       </div>
     );
   }
