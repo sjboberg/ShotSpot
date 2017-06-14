d7aa3efd35ead2eb92614a674cabb1b89f3fd389
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-06-13 17:10:14 -0700

    fixed typo in classname

--- client/src/Filter.jsx
+++ client/src/Filter.jsx
@@ -14,7 +14,7 @@ class Filter extends React.Component {
     }) : console.log('Waiting to get data');
     return (
       <div className="explore-filter">
-        <select name='categories' className="ui fliid dropdown"value={this.props.initValue} onChange={this.props.handleChangeFilter}>
+        <select name='categories' className="ui fluid dropdown" multiple="" id="filter-button"value={this.props.initValue} onChange={this.props.handleChangeFilter}>
           {Object.keys(locNames).map((value, i) => { return (this.props.initValue !== value) ? <option key={i} value={value}>{value}</option> : <option key={i} value={value} selected  >{value}</option>})}
         </select>
       </div>
