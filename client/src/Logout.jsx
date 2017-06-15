a0d33ea48908149ab5e09135ecfeac6c35018a91
Farrah Bousetta <farrah.bousetta@gmail.com>
2017-06-15 16:19:41 -0700

    logout component

---
+++ client/src/Logout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Logout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+     <div id="logout">
+     <h1 id="bye-text"> See you soon! </h1>
+     </div>
+    )
+  }
+}
+
+export default Logout;
